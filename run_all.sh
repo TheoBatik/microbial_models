77415b6e7c2ad0997ff549c7dbd3163ed9b8027b
TheoBatik <theo.batik@gmail.com>
2023-03-13 13:53:52 +0200

    Add shell script that runs all mic models

---
+++ run_all.sh
@@ -0,0 +1,6 @@
+pipenv run python 1a_PC8R7N2_1.py
+pipenv run python 1b_PC8R7N2_1.py
+pipenv run python 2_FBOC10N2_7.py
+pipenv run python 3_FBOC9N2_6.py
+pipenv run python 4_F_caldus.py 
+pipenv run python 5_A_niger.py
